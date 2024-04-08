44d2ced2c19cf5a84500ce3f436791340ab22b18
ED <ed_kuba@mail.ru>
2024-04-08 11:58:47 +0300

    next

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
-import React from 'react';
-import { Block } from './Block';
-import './index.scss';
+import React from "react";
+import { Block } from "./Block";
+import "./index.scss";
 
 function App() {
+  const [fromCurrency, setFromCurrency] = React.useState("RUB");
+  const [toCurrency, setToCurrency] = React.useState("USD");
+  const [fromPrice, setFromPrice] = React.useState(0);
+  const [toPrice, setToPrice] = React.useState(0);
+
+  const [rates, setRates] = React.useState({});
+
+  React.useEffect(() => {
+	fetch(
+	  "https://openexchangerates.org/api/latest.json?app_id=165999ebcd6240da8befd39277a5b2f4"
+	)
+	  .then((res) => res.json())
+	  .then((json) => {
+		setRates(json.rates);
+		console.log(json.rates);
+	  })
+	  .catch((err) => {
+		console.warn(err);
+		alert("Не удалось получить курсы валют");
+	  });
+  }, []);
+
+  const onChangeFromPrice = (value) => {
+	const price = value / rates[fromCurrency];
+	const result = price * rates[toCurrency];
+	setToPrice(result);
+	setFromPrice(value);
+  };
+
+  const onChangeToPrice = (value) => {
+	setToPrice(value);
+  };
+
   return (
-    <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
-    </div>
+	<div className="App">
+	  <Block
+		value={fromPrice}
+		currency={fromCurrency}
+		// 1:19
+		onChangeCurrency={setFromCurrency}
+		onChangeValue={onChangeFromPrice}
+	  />
+	  <Block
+		value={toPrice}
+		currency={toCurrency}
+		onChangeCurrency={setToCurrency}
+		onChangeValue={onChangeToPrice}
+	  />
+	</div>
   );
 }
 
